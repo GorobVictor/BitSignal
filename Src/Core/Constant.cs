d49c30cec47f1fe2d0ef5cef63a02f2f61b70574
Victor Horobchuk <Gorobchuk333@gmail.com>
2025-05-06 21:12:07 +0300

    feat: add Core

---
+++ Src/Core/Constant.cs
@@ -0,0 +1,12 @@
+﻿namespace Core;
+
+public class Constant
+{
+    public static readonly byte[] JwtSecret = "argfdsfaefadsfsdfdfaedfasdfafdsfadsfawadsfadfadsf"u8.ToArray();
+
+    public static readonly string JwtUserId = "JwtUserId";
+
+    public const string Issuer = "BitSignal";
+
+    public const string Audience = "BitSignalWebClient";
+}
