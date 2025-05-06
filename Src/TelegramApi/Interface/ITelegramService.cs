12fd7ccaaf994c1bc2bf296f3463537a400af526
Victor Horobchuk <Gorobchuk333@gmail.com>
2025-05-07 00:02:38 +0300

    feat: add telegram api

---
+++ Src/TelegramApi/Interface/ITelegramService.cs
@@ -0,0 +1,6 @@
+﻿namespace TelegramApi.Interface;
+
+public interface ITelegramService
+{
+    Task SendMessage(string? coinName, string? price);
+}
---
+++ Src/TelegramApi/Service/TelegramService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using TelegramApi.Interface;
+
+namespace TelegramApi.Service;
+
+public class TelegramService(IConfiguration configuration) : ITelegramService
+{
+    private readonly ITelegramBotClient _telegramService =
+        new TelegramBotClient(configuration.GetValue<string>("Telegram:ApiKey")!);
+
+    private readonly string _channelId =
+        configuration.GetValue<string>("Telegram:ChannelId")!;
+
+    private readonly string _adminName =
+        configuration.GetValue<string>("Telegram:AdminName")!;
+
+    public async Task SendMessage(string? coinName, string? price)
+    {
+        if(coinName is null || price is null) return;
+        var message = $"<blockquote>Name: {coinName}</blockquote>\n" +
+                      $"<blockquote>Price: {price}</blockquote>\n" +
+                      $"<blockquote>Price: {price}</blockquote>";
+        await this._telegramService.SendMessage(this._channelId, message, ParseMode.Html);
+    }
+}
